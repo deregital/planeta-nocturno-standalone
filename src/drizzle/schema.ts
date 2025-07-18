443a0feaa30346654c0e29f6281ac88560688030
nicofishman <74215078+nicofishman@users.noreply.github.com.>
2025-07-18 14:55:23 -0300

    feat: next auth cositas

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,19 +1,8 @@
-import { pgTable, uniqueIndex, uuid, text, timestamp, foreignKey, primaryKey, integer, boolean } from "drizzle-orm/pg-core"
+import { pgTable, uniqueIndex, foreignKey, text, uuid, timestamp, integer, boolean, doublePrecision, primaryKey, pgEnum } from "drizzle-orm/pg-core"
 import { sql } from "drizzle-orm"
 
+export const role = pgEnum("Role", ['ADMIN', 'STAFF'])
 
-
-export const user = pgTable("user", {
-	id: uuid().defaultRandom().primaryKey().notNull(),
-	name: text().notNull(),
-	email: text().notNull(),
-	emailVerified: timestamp({ withTimezone: true, mode: 'string' }),
-	image: text(),
-	createdAt: timestamp({ withTimezone: true, mode: 'string' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
-	password: text().notNull(),
-}, (table) => [
-	uniqueIndex("user_email_key").using("btree", table.email.asc().nullsLast().op("text_ops")),
-]);
 
 export const session = pgTable("session", {
 	sessionToken: text().notNull(),
@@ -27,6 +16,113 @@ export const session = pgTable("session", {
 			foreignColumns: [user.id],
 			name: "session_userId_fkey"
 		}).onUpdate("cascade").onDelete("cascade"),
+]);
+
+export const user = pgTable("user", {
+	id: uuid().defaultRandom().primaryKey().notNull(),
+	name: text().notNull(),
+	password: text().notNull(),
+	email: text().notNull(),
+	emailVerified: timestamp({ withTimezone: true, mode: 'string' }),
+	image: text(),
+	fullName: text().notNull(),
+	role: role().notNull(),
+	createdAt: timestamp({ withTimezone: true, mode: 'string' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
+}, (table) => [
+	uniqueIndex("user_email_key").using("btree", table.email.asc().nullsLast().op("text_ops")),
+]);
+
+export const event = pgTable("event", {
+	id: uuid().defaultRandom().primaryKey().notNull(),
+	name: text().notNull(),
+	description: text().notNull(),
+	coverImageUrl: text().notNull(),
+	slug: text().notNull(),
+	startingDate: timestamp({ precision: 3, mode: 'string' }).notNull(),
+	endingDate: timestamp({ precision: 3, mode: 'string' }).notNull(),
+	minAge: integer(),
+	locationId: uuid().notNull(),
+	categoryId: uuid().notNull(),
+	createdAt: timestamp({ withTimezone: true, mode: 'string' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
+}, (table) => [
+	foreignKey({
+			columns: [table.locationId],
+			foreignColumns: [location.id],
+			name: "event_locationId_fkey"
+		}).onUpdate("cascade").onDelete("restrict"),
+	foreignKey({
+			columns: [table.categoryId],
+			foreignColumns: [eventCategory.id],
+			name: "event_categoryId_fkey"
+		}).onUpdate("cascade").onDelete("restrict"),
+]);
+
+export const eventCategory = pgTable("eventCategory", {
+	id: uuid().defaultRandom().primaryKey().notNull(),
+	name: text().notNull(),
+	createdAt: timestamp({ withTimezone: true, mode: 'string' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
+});
+
+export const emmitedTicket = pgTable("emmitedTicket", {
+	id: uuid().defaultRandom().primaryKey().notNull(),
+	fullName: text().notNull(),
+	age: integer().notNull(),
+	dni: text().notNull(),
+	mail: text().notNull(),
+	phoneNumber: text().notNull(),
+	instagram: text(),
+	birthDate: text().notNull(),
+	paidOnLocation: boolean().notNull(),
+	scanned: boolean().default(false).notNull(),
+	scannedAt: timestamp({ precision: 3, mode: 'string' }),
+	scannedByUserId: uuid().notNull(),
+	eventId: uuid().notNull(),
+	ticketTypeId: uuid().notNull(),
+	createdAt: timestamp({ withTimezone: true, mode: 'string' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
+}, (table) => [
+	uniqueIndex("emmitedTicket_dni_key").using("btree", table.dni.asc().nullsLast().op("text_ops")),
+	foreignKey({
+			columns: [table.scannedByUserId],
+			foreignColumns: [user.id],
+			name: "emmitedTicket_scannedByUserId_fkey"
+		}).onUpdate("cascade").onDelete("restrict"),
+	foreignKey({
+			columns: [table.eventId],
+			foreignColumns: [event.id],
+			name: "emmitedTicket_eventId_fkey"
+		}).onUpdate("cascade").onDelete("restrict"),
+	foreignKey({
+			columns: [table.ticketTypeId],
+			foreignColumns: [ticketType.id],
+			name: "emmitedTicket_ticketTypeId_fkey"
+		}).onUpdate("cascade").onDelete("restrict"),
+]);
+
+export const location = pgTable("location", {
+	id: uuid().defaultRandom().primaryKey().notNull(),
+	name: text().notNull(),
+	adress: text().notNull(),
+	googleMapsUrl: text().notNull(),
+	capacity: integer().notNull(),
+	createdAt: timestamp({ withTimezone: true, mode: 'string' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
+});
+
+export const ticketType = pgTable("ticketType", {
+	id: uuid().defaultRandom().primaryKey().notNull(),
+	name: text().notNull(),
+	description: text().notNull(),
+	price: doublePrecision(),
+	maxAvailable: integer().notNull(),
+	maxPerPurchase: integer().notNull(),
+	scanLimit: timestamp({ precision: 3, mode: 'string' }),
+	eventId: uuid().notNull(),
+	createdAt: timestamp({ withTimezone: true, mode: 'string' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
+}, (table) => [
+	foreignKey({
+			columns: [table.eventId],
+			foreignColumns: [event.id],
+			name: "ticketType_eventId_fkey"
+		}).onUpdate("cascade").onDelete("restrict"),
 ]);
 
 export const verificationToken = pgTable("verificationToken", {
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -2,8 +2,14 @@ import NextAuth from 'next-auth';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { db } from '@/drizzle';
 import { eq } from 'drizzle-orm';
-import { user as userSchema } from '@/drizzle/schema';
 import { compare } from 'bcrypt';
+import { userSchema } from './schemas/user';
+import { user as userTable } from '@/drizzle/schema';
+
+const credentialsSchema = userSchema.pick({
+  name: true,
+  password: true,
+});
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
@@ -14,26 +20,69 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       name: 'Credentials',
       credentials: {
         email: {
-          label: 'Email',
-          type: 'email',
-          placeholder: 'jsmith@example.com',
+          label: 'Username',
+          type: 'text',
+          placeholder: 'Nombre de usuario',
         },
         password: { label: 'Password', type: 'password' },
       },
       authorize: async (credentials) => {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error('Missing email or password');
-        }
+        const { name, password } = credentialsSchema.parse(credentials);
         const user = await db.query.user.findFirst({
-          where: eq(userSchema.email, credentials.email as string),
+          where: eq(userTable.name, name as string),
         });
         if (!user) throw new Error('User not found');
-        if (!(await compare(credentials.password as string, user.password)))
+        if (!(await compare(password as string, user.password)))
           throw new Error('Invalid password');
-        return user;
+        return {
+          ...user,
+          emailVerified: user.emailVerified
+            ? new Date(user.emailVerified)
+            : null,
+        };
       },
     },
   ],
+  callbacks: {
+    async session({ session, token, user }) {
+      if (user) {
+        session.user = {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          emailVerified: user.emailVerified,
+          role: user.role,
+          fullName: user.fullName,
+          image: user.image,
+        };
+      } else {
+        session.user = {
+          id: token.sub as string,
+          name: token.name as string,
+          email: token.email as string,
+          emailVerified: token.emailVerified as Date | null,
+          role: token.role as string,
+          fullName: token.fullName as string,
+          image: token.image as string | null,
+        };
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+        token.role = user.role;
+        token.fullName = user.fullName;
+        token.image = user.image;
+      }
+      return token;
+    },
+  },
+  session: {
+    strategy: 'jwt',
+  },
   pages: {
     signIn: '/login',
   },
