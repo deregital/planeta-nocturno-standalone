5425b91c508b658ece3e27da8fa95a5fea97c87c
sebipochi <sebipochi@gmail.com>
2025-08-18 01:03:01 -0300

    fix: page => client, client => page

--- src/app/admin/event/client.tsx
+++ src/app/admin/event/client.tsx
@@ -1,5 +1,65 @@
-import Client from './page';
+'use client';
+import { Button } from '@/components/ui/button';
+import { ticketTypesTranslation } from '@/lib/translations';
+import { type RouterOutputs } from '@/server/routers/app';
+import { format } from 'date-fns';
+import { useRouter } from 'next/navigation';
+import React from 'react';
 
-export default function Events() {
-  return <Client />;
+export default function Client({
+  events,
+}: {
+  events: RouterOutputs['events']['getAll'];
+}) {
+  const router = useRouter();
+
+  return (
+    <div className='w-full'>
+      <h1 className='text-4xl'>Gestor de Eventos</h1>
+
+      <ul className='gap-4'>
+        {events.map((event, index) => (
+          <React.Fragment key={index}>
+            <li
+              key={index}
+              className='grid grid-cols-4 font-bold bg-pn-accent/30'
+            >
+              <p>Evento: </p>
+              <p>{event.name}</p>
+              <p>{format(event.startingDate, 'dd/MM/yyyy HH:mm')}</p>
+              <p>{event.slug}</p>
+            </li>
+            <li
+              key={index + '-label'}
+              className='grid grid-cols-4 gap-4 border font-bold bg-pn-gray/30'
+            >
+              <p>Tipo de entrada</p>
+              <p>Nombre</p>
+              <p>Descripción</p>
+              <p>Max por compra</p>
+            </li>
+            <ul>
+              {event.ticketTypes.map((type, index) => {
+                const { text } = ticketTypesTranslation[type.category];
+                return (
+                  <li key={index + '-' + type.id} className='grid grid-cols-4'>
+                    <p>{text}</p>
+                    <p>{type.name}</p>
+                    <p>{type.price}</p>
+                    <p>{type.maxPerPurchase}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </React.Fragment>
+        ))}
+      </ul>
+      <Button
+        className='flex mx-4 justify-self-end'
+        onClick={() => router.push('/admin/event/create')}
+      >
+        Crear evento
+      </Button>
+    </div>
+  );
 }
--- src/app/admin/event/page.tsx
+++ src/app/admin/event/page.tsx
@@ -1,64 +1,12 @@
-'use client';
-import { Button } from '@/components/ui/button';
-import { ticketTypesTranslation } from '@/lib/translations';
-import { trpc } from '@/server/trpc/client';
-import { format } from 'date-fns';
-import { Loader } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import React from 'react';
+import { trpc } from '@/server/trpc/server';
+import Client from './client';
 
-export default function Client() {
-  const { data: events, isLoading } = trpc.events.getAll.useQuery();
+export default async function Page() {
+  const events = await trpc.events.getAll();
 
-  const router = useRouter();
-
-  return isLoading || !events ? (
-    <Loader />
-  ) : (
-    <div className='w-full'>
-      <ul className='gap-4'>
-        {events.map((event, index) => (
-          <React.Fragment key={index}>
-            <li
-              key={index}
-              className='grid grid-cols-4 font-bold bg-pn-accent/30'
-            >
-              <p>Evento: </p>
-              <p>{event.name}</p>
-              <p>{format(event.startingDate, 'dd/MM/yyyy HH:mm')}</p>
-              <p>{event.slug}</p>
-            </li>
-            <li
-              key={index + '-label'}
-              className='grid grid-cols-4 gap-4 border font-bold bg-pn-gray/30'
-            >
-              <p>Tipo de entrada</p>
-              <p>Nombre</p>
-              <p>Descripción</p>
-              <p>Max por compra</p>
-            </li>
-            <ul>
-              {event.ticketTypes.map((type, index) => {
-                const { text } = ticketTypesTranslation[type.category];
-                return (
-                  <li key={index + '-' + type.id} className='grid grid-cols-4'>
-                    <p>{text}</p>
-                    <p>{type.name}</p>
-                    <p>{type.price}</p>
-                    <p>{type.maxPerPurchase}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          </React.Fragment>
-        ))}
-      </ul>
-      <Button
-        className='flex mx-4 justify-self-end'
-        onClick={() => router.push('/admin/event/create')}
-      >
-        Crear evento
-      </Button>
+  return (
+    <div>
+      <Client events={events} />
     </div>
   );
 }
