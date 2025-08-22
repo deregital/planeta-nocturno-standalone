5f8b2c3aa2bbe9c7dcc6de73f8e400c193e1d58a
nicofishman <nicourifishman@gmail.com>
2025-08-22 16:29:32 -0300

    fix: UI EmitTicketModal

--- src/components/event/individual/EmitTicketModal.tsx
+++ src/components/event/individual/EmitTicketModal.tsx
@@ -5,6 +5,7 @@ import {
   DialogContent,
   DialogTrigger,
   DialogTitle,
+  DialogDescription,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { type RouterOutputs } from '@/server/routers/app';
@@ -32,6 +33,7 @@ export function EmitTicketModal({
 }) {
   const [open, setOpen] = useState(false);
   const [error, setError] = useState<Record<string, string>>({});
+  const [emitTicketLoading, setEmitTicketLoading] = useState(false);
   const [birthDate, setBirthDate] = useState(new Date());
   const [phoneNumber, setPhoneNumber] = useState('');
   const formRef = useRef<HTMLFormElement>(null);
@@ -71,7 +73,7 @@ export function EmitTicketModal({
       gender: formData.get('gender') as 'male' | 'female' | 'other',
       phoneNumber: phoneNumber,
       paidOnLocation: formData.get('paidOnLocation') === 'on',
-      ticketTypeId: formData.get('ticketTypeId') as string,
+      ticketTypeId: selectedTicketTypeId,
       instagram: (formData.get('instagram') as string) || undefined,
     };
 
@@ -90,7 +92,9 @@ export function EmitTicketModal({
       return;
     }
 
+    setEmitTicketLoading(true);
     await emitTicket(ticketData);
+    setEmitTicketLoading(false);
 
     setOpen(false);
     setError({});
@@ -105,7 +109,15 @@ export function EmitTicketModal({
   }
 
   return (
-    <Dialog open={open} onOpenChange={setOpen} modal={true}>
+    <Dialog
+      open={open}
+      onOpenChange={(open) => {
+        setOpen(open);
+        if (!open) {
+          setError({});
+        }
+      }}
+    >
       <DialogTrigger asChild>
         <Button className='flex gap-x-5' variant='outline'>
           Emitir ticket
@@ -114,144 +126,168 @@ export function EmitTicketModal({
       </DialogTrigger>
       <DialogContent>
         <DialogTitle className='text-lg font-semibold'>
-          Escanear ticket
+          Emitir ticket para {event?.name}
         </DialogTitle>
+        <DialogDescription hidden>Emision manual de ticket</DialogDescription>
         <div>
           <form
             ref={formRef}
             className='flex flex-col gap-3'
             onSubmit={handleSubmit}
           >
-            <InputWithLabel
-              label='Nombre completo'
-              id='name'
-              name='fullName'
-              defaultValue=''
-              required={true}
-              error={error.fullName}
-            />
-            <InputWithLabel
-              label='DNI'
-              id='dni'
-              name='dni'
-              defaultValue=''
-              required={true}
-              error={error.dni}
-            />
-            <InputDateWithLabel
-              label='Fecha de nacimiento'
-              id='birthDate'
-              selected={birthDate}
-              onChange={(date) => {
-                if (date) {
-                  setBirthDate(date);
-                }
-              }}
-              required={true}
-              error={error.birthDate}
-            />
-            <InputWithLabel
-              label='Email'
-              id='email'
-              name='mail'
-              type='email'
-              defaultValue=''
-              required={true}
-              error={error.mail}
-            />
-            <div className='flex flex-col gap-1'>
-              <Label
-                className='pl-1 text-pn-accent gap-0.5'
-                htmlFor={`phoneNumber`}
-              >
-                Número de teléfono<span className='text-red-500'>*</span>
-              </Label>
-              <PhoneInput
-                value={phoneNumber}
-                onChange={(v) => {
-                  setPhoneNumber(v || '');
-                }}
-                labels={esPhoneLocale}
-                defaultCountry='AR'
-                className={cn(
-                  '[&_[data-slot="input"]]:border-pn-gray',
-                  error.phoneNumber &&
-                    '[&_[data-slot="input"]]:border-red-500 [&_[data-slot="input"]]:border-2',
-                )}
-                inputComponent={Input}
-                error={error.phoneNumber}
+            <FormRow>
+              <InputWithLabel
+                label='Nombre completo'
+                id='name'
+                name='fullName'
+                defaultValue=''
+                required={true}
+                error={error.fullName}
               />
-              {error.phoneNumber && (
-                <p className='pl-1 font-bold text-xs text-red-500'>
-                  {error.phoneNumber}
-                </p>
-              )}
-            </div>
-            <SelectWithLabel
-              className='w-full'
-              label='Género'
-              id='gender'
-              name='gender'
-              defaultValue='male'
-              values={[
-                { label: 'Masculino', value: 'male' },
-                { label: 'Femenino', value: 'female' },
-                { label: 'Otro', value: 'other' },
-              ]}
-              onValueChange={(value) => {
-                const select = formRef.current?.querySelector(
-                  '[name="gender"]',
-                ) as HTMLSelectElement;
-                if (select) {
-                  select.value = value;
-                }
-              }}
-              required={true}
-              error={error.gender}
-            />
-            <InputWithLabel
-              label='Instagram'
-              id='instagram'
-              name='instagram'
-              defaultValue=''
-              error={error.instagram}
-            />
-            <SelectWithLabel
-              className='w-full'
-              label='Tipo de entrada'
-              id='ticketTypeId'
-              name='ticketTypeId'
-              defaultValue=''
-              values={event!.ticketTypes.map((type) => ({
-                label: type.name,
-                value: type.id,
-              }))}
-              onValueChange={(value) => {
-                setSelectedTicketTypeId(value);
-                const select = formRef.current?.querySelector(
-                  '[name="ticketTypeId"]',
-                ) as HTMLSelectElement;
-                if (select) {
-                  select.value = value;
-                }
-              }}
-              required={true}
-              error={error.ticketTypeId}
-            />
-            <InputWithLabel
-              className='[&_[data-slot="input"]]:w-7 [&_[data-slot="input"]]:ml-1'
-              type='checkbox'
-              label='Pagada en local?'
-              id='paidOnLocation'
-              name='paidOnLocation'
-              disabled={selectedTicketType?.category === 'FREE'}
-              defaultChecked={true}
-              error={error.paidOnLocation}
-            />
-            <Button type='submit'>Emitir ticket</Button>
+            </FormRow>
+            <FormRow>
+              <InputWithLabel
+                label='DNI'
+                id='dni'
+                name='dni'
+                defaultValue=''
+                required={true}
+                error={error.dni}
+              />
+              <InputWithLabel
+                label='Email'
+                id='email'
+                name='mail'
+                type='email'
+                defaultValue=''
+                required={true}
+                error={error.mail}
+              />
+            </FormRow>
+
+            <FormRow>
+              <div className='flex flex-col gap-1'>
+                <Label
+                  className='pl-1 text-pn-accent gap-0.5'
+                  htmlFor={`phoneNumber`}
+                >
+                  Número de teléfono<span className='text-red-500'>*</span>
+                </Label>
+                <PhoneInput
+                  value={phoneNumber}
+                  onChange={(v) => {
+                    setPhoneNumber(v || '');
+                  }}
+                  labels={esPhoneLocale}
+                  defaultCountry='AR'
+                  className={cn(
+                    '[&_[data-slot="input"]]:border-pn-gray',
+                    error.phoneNumber &&
+                      '[&_[data-slot="input"]]:border-red-500 [&_[data-slot="input"]]:border-2',
+                  )}
+                  inputComponent={Input}
+                  error={error.phoneNumber}
+                />
+                {error.phoneNumber && (
+                  <p className='pl-1 font-bold text-xs text-red-500'>
+                    {error.phoneNumber}
+                  </p>
+                )}
+              </div>
+              <InputWithLabel
+                label='Instagram'
+                id='instagram'
+                name='instagram'
+                defaultValue=''
+                error={error.instagram}
+              />
+            </FormRow>
+            <FormRow>
+              <SelectWithLabel
+                className='w-full'
+                label='Género'
+                id='gender'
+                name='gender'
+                defaultValue='male'
+                values={[
+                  { label: 'Masculino', value: 'male' },
+                  { label: 'Femenino', value: 'female' },
+                  { label: 'Otro', value: 'other' },
+                ]}
+                onValueChange={(value) => {
+                  const select = formRef.current?.querySelector(
+                    '[name="gender"]',
+                  ) as HTMLSelectElement;
+                  if (select) {
+                    select.value = value;
+                  }
+                }}
+                required={true}
+                error={error.gender}
+              />
+              <InputDateWithLabel
+                label='Fecha de nacimiento'
+                id='birthDate'
+                selected={birthDate}
+                onChange={(date) => {
+                  if (date) {
+                    setBirthDate(date);
+                  }
+                }}
+                required={true}
+                error={error.birthDate}
+              />
+            </FormRow>
+
+            <FormRow>
+              <SelectWithLabel
+                className='w-full'
+                label='Tipo de entrada'
+                id='ticketTypeId'
+                name='ticketTypeId'
+                defaultValue=''
+                values={event!.ticketTypes.map((type) => ({
+                  label: `${type.name} - $${type.price}`,
+                  value: type.id,
+                }))}
+                onValueChange={(value) => {
+                  setSelectedTicketTypeId(value);
+                  const select = formRef.current?.querySelector(
+                    '[name="ticketTypeId"]',
+                  ) as HTMLSelectElement;
+                  if (select) {
+                    select.value = value;
+                  }
+                }}
+                required={true}
+                error={error.ticketTypeId}
+              />
+              <InputWithLabel
+                className='[&_[data-slot="input"]]:w-7 [&_[data-slot="input"]]:ml-1'
+                type='checkbox'
+                label='Pagada en local?'
+                id='paidOnLocation'
+                name='paidOnLocation'
+                disabled={selectedTicketType?.category === 'FREE'}
+                defaultChecked={true}
+                error={error.paidOnLocation}
+              />
+            </FormRow>
+
+            <Button type='submit' disabled={emitTicketLoading}>
+              Emitir ticket
+            </Button>
           </form>
         </div>
       </DialogContent>
     </Dialog>
+  );
+}
+
+function FormRow({ children }: { children: React.ReactNode }) {
+  return (
+    <div className='flex flex-row gap-3 items-start w-full [&>*]:flex-1'>
+      {children}
+    </div>
   );
 }
