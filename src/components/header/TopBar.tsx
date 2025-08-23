e1b99daf7a7c2d49dcb43cb02281b9621f91e2d8
sebipochi <sebipochi@gmail.com>
2025-08-23 09:15:08 -0300

    feat: add different sizes

--- src/components/header/TopBar.tsx
+++ src/components/header/TopBar.tsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 function TopBar() {
   return (
     <div className='bg-pn-background w-full lg:h-[16vh] md:h-[12vh] h-[8vh] flex items-center pl-[20px] md:pl-[50px]'>
-      <InstanceLogo />
+      <InstanceLogo size='lg' />
       <Navbar />
     </div>
   );
