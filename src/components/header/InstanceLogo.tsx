4b1238fdb716001d7ea7ff19d34ac9d640d9369e
sebipochi <sebipochi@gmail.com>
2025-08-23 08:10:03 -0300

    fix: get INSTANCE_NAME correctly where needed

--- src/components/header/InstanceLogo.tsx
+++ src/components/header/InstanceLogo.tsx
@@ -1,4 +1,7 @@
-function InstanceLogo({ firstWord, rest }: { firstWord: string; rest: string }) {
+function InstanceLogo() {
+  const [firstWord, ...rest] =
+    process.env.NEXT_PUBLIC_INSTANCE_NAME!.split(' ');
+
   return (
     <h1 className='text-[1.5rem] md:text-[2.5rem] lg:text-[4rem] font-sans font-bold leading-[80px]'>
       <span className='text-pn-accent'>{firstWord}</span>
