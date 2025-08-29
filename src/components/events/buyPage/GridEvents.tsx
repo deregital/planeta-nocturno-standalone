9de7f06e4685dc8b05063b9793d12dab69bf7e98
nicofishman <nicourifishman@gmail.com>
2025-08-29 11:17:59 -0300

    fix: home layout

--- src/components/events/buyPage/GridEvents.tsx
+++ src/components/events/buyPage/GridEvents.tsx
@@ -1,9 +1,42 @@
 'use client';
+import { parseAsString, useQueryState } from 'nuqs';
+import { useMemo } from 'react';
+import { isWithinInterval } from 'date-fns';
+
 import { trpc } from '@/server/trpc/client';
 import EventCardContainer from '@/components/events/buyPage/EventCardContainer';
+import { dateRanges } from '@/components/events/buyPage/EventFilter';
 
 function GridEvents() {
   const { data, isLoading } = trpc.events.getActive.useQuery();
+  const [dateRange] = useQueryState('date', parseAsString);
+  const [search] = useQueryState('q', parseAsString);
+
+  const selectedDateRange = useMemo(() => {
+    if (!dateRange) return null;
+    return dateRanges.find((range) => range.id === dateRange);
+  }, [dateRange]);
+
+  const filteredEvents = useMemo(() => {
+    const searchValue = search
+      ?.toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+
+    return data
+      ?.filter((event) => {
+        if (!searchValue) return true;
+        return event.name.toLowerCase().includes(searchValue);
+      })
+      .filter((event) => {
+        console.log(selectedDateRange);
+        if (!selectedDateRange) return true;
+        return isWithinInterval(event.startingDate, {
+          start: selectedDateRange?.from,
+          end: selectedDateRange?.to,
+        });
+      });
+  }, [data, search, selectedDateRange]);
 
   if (isLoading) {
     return (
@@ -22,10 +55,10 @@ function GridEvents() {
   }
 
   return (
-    <div className='max-w-full mx-5 md:mx-[3rem] py-4 sm:py-8'>
-      <div className='flex flex-wrap gap-6 items-center'>
-        {data.map((event) => (
-          <div key={event.id}>
+    <div className='max-w-full py-4 sm:py-8 mb-20'>
+      <div className='grid grid-cols-2 md:grid-cols-3 place-content-center lg:grid-cols-3 xl:grid-cols-4 gap-6'>
+        {filteredEvents?.map((event) => (
+          <div key={event.id} className='justify-self-center'>
             <EventCardContainer event={event} />
           </div>
         ))}
