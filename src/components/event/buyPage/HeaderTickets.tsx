96fe183cffba3e97999d77a07f9cb8286b022ef0
nicofishman <74215078+nicofishman@users.noreply.github.com.>
2025-09-10 01:24:52 -0300

    feat: square event picture

--- src/components/event/buyPage/HeaderTickets.tsx
+++ src/components/event/buyPage/HeaderTickets.tsx
@@ -30,38 +30,27 @@ function HeaderTickets({
     : '/Foto.png';
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-12 w-full h-full overflow-hidden'>
+    <div className='grid relative grid-cols-1 md:grid-cols-12 w-full h-full overflow-hidden'>
       {/* Lado izquierdo - Nombre del evento y fecha */}
-      <div className='col-span-1 md:col-span-5 px-4 py-2 md:px-4 md:py-3 lg:px-6 lg:py-4 flex flex-col justify-center overflow-hidden relative'>
+      <div className='col-span-1 md:col-span-5 px-4 py-2 md:px-4 md:py-3 lg:px-6 lg:py-4 flex flex-col justify-center overflow-hidden relative border-b border-stroke md:border-b-0'>
         {/* Fondo solo visible en móvil (menos que md) */}
         <div
-          className='absolute inset-0 bg-cover bg-center md:hidden'
+          className='absolute right-0 bg-cover aspect-square bg-center h-full md:hidden'
           style={{ backgroundImage: `url(${backgroundImageUrl})` }}
         ></div>
 
-        {/* Overlay para mejorar legibilidad del texto en mobile */}
-        <div className='absolute inset-0 bg-gradient-to-t from-black/40 to-black/20 md:hidden'></div>
-
         <div className='relative z-10'>
-          <h1 className='text-xl md:text-2xl lg:text-3xl font-base text-white md:text-black line-clamp-2 md:line-clamp-1'>
+          <h1 className='text-xl md:text-2xl lg:text-3xl font-base text-black line-clamp-2 md:line-clamp-1'>
             {event.name}
           </h1>
-          <p className='text-sm lg:text-base whitespace-nowrap text-white/80 md:text-accent capitalize mt-2 overflow-hidden text-ellipsis'>
+          <p className='text-sm lg:text-base whitespace-nowrap text-black md:text-accent capitalize mt-2 overflow-hidden text-ellipsis'>
             {formattedDate}
           </p>
         </div>
       </div>
 
-      {/* Medio - Banner del evento (solo visible en desktop) */}
-      <div className='hidden md:block md:col-span-5 h-full'>
-        <div
-          className='w-full h-full bg-cover bg-center'
-          style={{ backgroundImage: `url(${backgroundImageUrl})` }}
-        ></div>
-      </div>
-
       {/* Lado derecho - Ubicación */}
-      <div className='col-span-1 md:col-span-2 px-4 py-2 md:px-4 md:py-3 lg:px-6 lg:py-4 flex items-center justify-start md:border-l border-stroke overflow-hidden'>
+      <div className='col-span-1 flex-1 md:col-span-6 px-4 py-2 md:px-4 md:py-3 lg:px-6 lg:py-4 flex items-center justify-start md:border-l border-stroke overflow-hidden'>
         <div className='flex items-start min-w-0'>
           <div className='flex items-center justify-center mr-2'>
             <MapPin
@@ -78,6 +67,14 @@ function HeaderTickets({
             </p>
           </div>
         </div>
+      </div>
+
+      {/* Medio - Banner del evento (solo visible en desktop) */}
+      <div className='hidden absolute right-0 md:block md:col-span-1 h-full w-fit'>
+        <div
+          className='aspect-square h-full max-h-25 bg-cover bg-center'
+          style={{ backgroundImage: `url(${backgroundImageUrl})` }}
+        ></div>
       </div>
     </div>
   );
