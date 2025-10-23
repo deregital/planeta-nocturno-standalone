d5cb55984c0d406c14a02f40a8bda65f1a1e901f
Nicolás Fishman <nicourifishman@gmail.com>
2025-10-23 18:42:14 -0300

    feat: error if no organizers selected in invitation mode

--- src/components/event/create/EventInvitationTypeAction.tsx
+++ src/components/event/create/EventInvitationTypeAction.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { toast } from 'sonner';
 
 import { useCreateEventStore } from '@/app/admin/event/create/provider';
 import { Tabs, TabsTrigger, TabsList, TabsContent } from '@/components/ui/tabs';
@@ -15,6 +16,7 @@ export function EventInvitationTypeAction({
   back: () => void;
 }) {
   const event = useCreateEventStore((state) => state.event);
+  const organizers = useCreateEventStore((state) => state.organizers);
   const resetOrganizers = useCreateEventStore((state) => state.resetOrganizers);
   const setEvent = useCreateEventStore((state) => state.setEvent);
 
@@ -24,6 +26,17 @@ export function EventInvitationTypeAction({
     setTab(value);
     setEvent({ inviteContidtion: value });
     resetOrganizers();
+  }
+
+  function handleNext() {
+    if (tab === 'INVITATION' && organizers.length === 0) {
+      toast.error(
+        'Debes seleccionar al menos un organizador en modo invitación',
+      );
+      return;
+    }
+
+    next();
   }
 
   return (
@@ -54,13 +67,7 @@ export function EventInvitationTypeAction({
             Volver
           </Button>
         )}
-        <Button
-          className='flex-1'
-          variant={'accent'}
-          onClick={() => {
-            next();
-          }}
-        >
+        <Button className='flex-1' variant={'accent'} onClick={handleNext}>
           Continuar
         </Button>
       </div>
