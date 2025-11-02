7a42e3e120e5bb3e6e1b27881ef7d52862c5da3f
Nicolás Fishman <nicourifishman@gmail.com>
2025-11-02 13:49:30 -0300

    fix: ticketXorganizer id

---
+++ prisma/migrations/20251102164833_fix_ticketxorganizer/migration.sql
@@ -0,0 +1,42 @@
+-- AlterTable
+ALTER TABLE "account" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "emittedTicket" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "event" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "eventCategory" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "eventXOrganizer" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "feature" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "location" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "session" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "ticketGroup" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "ticketType" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "ticketXOrganizer" ALTER COLUMN "code" SET DEFAULT upper(substr(md5(random()::text), 1, 6)),
+ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP,
+ADD CONSTRAINT "ticketXOrganizer_pkey" PRIMARY KEY ("eventId", "code");
+
+-- AlterTable
+ALTER TABLE "user" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP,
+ALTER COLUMN "birthDate" DROP DEFAULT,
+ALTER COLUMN "code" SET DEFAULT upper(substr(md5(random()::text), 1, 6)),
+ALTER COLUMN "dni" DROP DEFAULT,
+ALTER COLUMN "gender" DROP DEFAULT,
+ALTER COLUMN "phoneNumber" DROP DEFAULT;
