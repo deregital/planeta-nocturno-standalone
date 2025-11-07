1b5ade59faf183b77906b4aab1b1362ad571dc1e
Nicolás Fishman <nicourifishman@gmail.com>
2025-11-07 18:49:08 -0300

    fix: global sessionprovider

--- src/app/(backoffice)/admin/database/page.tsx
+++ src/app/(backoffice)/admin/database/page.tsx
@@ -1,14 +1,8 @@
-import { SessionProvider } from 'next-auth/react';
-
 import Client from '@/app/(backoffice)/admin/database/client';
 import { trpc } from '@/server/trpc/server';
 
 export default async function Page() {
   const buyers = await trpc.emittedTickets.getAllUniqueBuyer();
 
-  return (
-    <SessionProvider>
-      <Client buyers={buyers} />
-    </SessionProvider>
-  );
+  return <Client buyers={buyers} />;
 }
