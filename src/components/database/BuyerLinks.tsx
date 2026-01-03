df35e2a2038854893605a7ecac2b0f1b603f7137
sebipochi <sebipochi@gmail.com>
2026-01-03 02:22:34 -0300

    refactor: remove unnecessary divs

--- src/components/database/BuyerLinks.tsx
+++ src/components/database/BuyerLinks.tsx
@@ -15,45 +15,39 @@ export default function BuyerLinks({
   mail,
 }: BuyerLinksParams) {
   return (
-    <div className='flex justify-center items-center gap-4 ml-8 [&>div]:w-9 [&>div]:h-9 [&>div]:inline-flex [&>div]:items-center [&>div]:justify-center [&>div]:border [&>div]:rounded-sm [&>div]:transition'>
+    <div className='flex justify-center items-center gap-4 ml-8 [&>a]:w-9 [&>a]:h-9 [&>a]:inline-flex [&>a]:items-center [&>a]:cursor-pointer [&>a]:justify-center [&>a]:border [&>a]:border-black [&>a]:rounded-sm [&>a]:transition'>
       {instagram && (
-        <div className='bg-[#DA00A4] hover:bg-[#DA00A4]/75'>
-          <a
-            href={`https://instagram.com/${instagram}`}
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='Instagram'
-            className='text-white '
-          >
-            <Instagram />
-          </a>
-        </div>
+        <a
+          href={`https://instagram.com/${instagram}`}
+          target='_blank'
+          rel='noopener noreferrer'
+          aria-label='Instagram'
+          className='text-white bg-[#DA00A4] hover:bg-[#DA00A4]/75'
+        >
+          <Instagram />
+        </a>
       )}
       {phoneNumber && (
-        <div className='bg-[#00C500] hover:bg-[#00C500]/75'>
-          <a
-            href={`https://wa.me/${phoneNumber}`}
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='WhatsApp'
-            className='text-white'
-          >
-            <WhatsApp />
-          </a>
-        </div>
+        <a
+          href={`https://wa.me/${phoneNumber}`}
+          target='_blank'
+          rel='noopener noreferrer'
+          aria-label='WhatsApp'
+          className='text-white bg-[#00C500] hover:bg-[#00C500]/75'
+        >
+          <WhatsApp />
+        </a>
       )}
       {mail && (
-        <div className='bg-[#DA0004] hover:bg-[#DA0004]/75'>
-          <a
-            href={`mailto:${mail}`}
-            target='_blank'
-            rel='noopener noreferrer'
-            aria-label='Email'
-            className='text-white'
-          >
-            <Mail />
-          </a>
-        </div>
+        <a
+          href={`mailto:${mail}`}
+          target='_blank'
+          rel='noopener noreferrer'
+          aria-label='Email'
+          className='text-white bg-[#DA0004] hover:bg-[#DA0004]/75'
+        >
+          <Mail />
+        </a>
       )}
     </div>
   );
