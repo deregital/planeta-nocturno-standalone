49f15b29271797dd12d81209153056af380c74bd
Nicolás Fishman <nicourifishman@gmail.com>
2026-01-11 21:21:33 -0300

    feat: add organizerId to event creation state and invitation ticket type

--- src/app/(backoffice)/admin/event/create/state.ts
+++ src/app/(backoffice)/admin/event/create/state.ts
@@ -333,6 +333,7 @@ export const createEventStore = (initState: EventState = initialState) => {
               maxSellDate: null,
               scanLimit: null,
               visibleInWeb: false,
+              organizerId: null,
             },
           ],
         };
--- src/server/schemas/ticket-type.ts
+++ src/server/schemas/ticket-type.ts
@@ -29,7 +29,6 @@ export const ticketTypeSchema = z.object({
     })
     .nullable(),
   organizerId: z
-    .string()
     .uuid()
     .nullable()
     .optional()
