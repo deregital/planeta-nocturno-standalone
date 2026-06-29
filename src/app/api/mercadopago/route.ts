43ef4cd89a6fef4baff908feba43a2d036dbb355
sebipochi <sebipochi@gmail.com>
2026-06-29 16:19:03 -0300

    remove unused files + revert mercadopago api changes

--- src/app/api/mercadopago/route.ts
+++ src/app/api/mercadopago/route.ts
@@ -1,34 +1,94 @@
+import { createHmac, timingSafeEqual } from 'crypto';
+
+import { Payment } from 'mercadopago';
 import { NextResponse } from 'next/server';
 
-import {
-  handleMercadoPagoWebhookNotification,
-  verifyMercadoPagoWebhookSignature,
-} from '@/server/services/mercadoPagoWebhook';
+import { mercadoPago } from '@/server/routers/mercado-pago';
+import { sendMailService } from '@/server/services/mail';
+import { sendNotificationService } from '@/server/services/notification';
+import { updateTicketGroupStatus } from '@/server/services/ticketGroup';
+import { trpc } from '@/server/trpc/server';
+
+function verifySignature(
+  signature: string,
+  request_id: string,
+  data_id: string,
+): boolean {
+  const ts = signature.split(',')[0]?.split('=')[1];
+  const v1 = signature.split(',')[1]?.split('=')[1];
+  const manifest = `id:${data_id};request-id:${request_id};ts:${ts?.trim()};`;
+  const secretKey = process.env.MP_SECRET_KEY!;
+  const signatureDecrypted = createHmac('sha256', secretKey)
+    .update(manifest)
+    .digest('hex');
+  // comparacion segura de signatures para evitar timing attacks
+  const a = Buffer.from(signatureDecrypted);
+  const b = Buffer.from(v1?.trim() ?? '');
+  const isValid = a.length === b.length && timingSafeEqual(a, b);
+  return isValid;
+}
 
 export async function POST(req: Request) {
   const body: { data: { id: string } } = await req.json();
   const signature = req.headers.get('x-signature');
   const requestId = req.headers.get('x-request-id');
-  const paymentId = body.data.id;
 
-  if (!signature || !requestId || !paymentId) {
+  if (!signature || !requestId) {
     return new NextResponse(null, { status: 400 });
   }
 
-  const isValid = verifyMercadoPagoWebhookSignature(
-    signature,
-    requestId,
-    paymentId,
-  );
+  const isValid = verifySignature(signature, requestId, body.data.id);
 
   if (!isValid) {
     return new NextResponse(null, { status: 403 });
   }
 
-  const result = await handleMercadoPagoWebhookNotification(paymentId);
+  const payment = await new Payment(mercadoPago).get({ id: body.data.id });
 
-  if (result.reason === 'not_found') {
+  if (!payment || !payment.external_reference) {
     return new NextResponse(null, { status: 404 });
+  }
+
+  if (payment.status === 'approved') {
+    // cambiar status de ticketGroup a pagado
+    if (payment.external_reference) {
+      await updateTicketGroupStatus(payment.external_reference, 'PAID');
+    }
+    const group = await trpc.ticketGroup.getById(payment.external_reference);
+
+    // crear pdf
+    const pdfs = await trpc.ticketGroup.generatePdfsByTicketGroupId(
+      payment.external_reference,
+    );
+
+    // enviar mail con los pdf de forma secuencial para evitar rate limits
+    if (!group.event.extraTicketData) {
+      await sendMailService({
+        eventName: group.event.name,
+        receiver: pdfs[0].ticket.mail,
+        subject: `¡Llegaron tus tickets para ${group.event.name}!`,
+        body: `Te esperamos.`,
+        attatchments: pdfs.map((pdf) => pdf.pdf.blob),
+      });
+    } else {
+      for (const pdf of pdfs) {
+        await sendMailService({
+          eventName: group.event.name,
+          receiver: pdf.ticket.mail,
+          subject: `¡Llegaron tus tickets para ${group.event.name}!`,
+          body: `Te esperamos.`,
+          attatchments: [pdf.pdf.blob],
+        });
+      }
+    }
+
+    if (group.event.emailNotification) {
+      await sendNotificationService({
+        eventName: group.event.name,
+        ticketGroupId: group.id,
+        email: group.event.emailNotification,
+      });
+    }
   }
 
   return new NextResponse(null, { status: 200 });
